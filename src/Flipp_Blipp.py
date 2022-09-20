2eeb375556ea6bb2e0f2a3715ac1249bf7bdb855
Björn Westerlund <motif@DESKTOP-A2QIC27.localdomain>
2022-09-20 19:10:05 +0200

    blipp

---
+++ src/Flipp_Blipp.py
@@ -0,0 +1,15 @@
+def flippBlipp(n):
+    
+    for x in range(1,n):
+        out =  ""
+        if x % 3 == 0:
+            out+="flipp"
+        if x % 5 == 0:
+            if out != "":
+                out+=" "
+            out+="blipp"
+        if out != "":
+            print(out)
+        else:
+            print(x)
+flippBlipp(100)
